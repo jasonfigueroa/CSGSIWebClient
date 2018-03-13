2686f28c6286217efdb657b0966896778b5d7291
Jason Figueroa <jason.figueroa2@gmail.com>
2018-03-12 22:02:04 -0500

    console app is now downloadable, adjust port number accordingly in site.js and _Layout.cshtml

--- CSGSIWebClient/wwwroot/js/site.js
+++ CSGSIWebClient/wwwroot/js/site.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
     $('.clickable').click(function () {
-        const baseUrl = "http://localhost:57602/matches/match";
+        const baseUrl = "http://localhost:49424/matches/match";
         const id = $(this)[0].id.split("__")[1];
         let url = `${baseUrl}/${id}`;
         window.location.href = url;
